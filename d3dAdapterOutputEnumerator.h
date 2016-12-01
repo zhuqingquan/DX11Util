d0f3e48a96199261b2411bdc3474250d77c68a30
zhuqingquan <zqq_222@163.com>
2016-12-01 21:54:15 +0800

    add func DXGI_getNearestAdapter

--- d3dAdapterOutputEnumerator.h
+++ d3dAdapterOutputEnumerator.h
@@ -7,6 +7,9 @@
 
 #include <DXGI.h>
 #include <vector>
+#include <Windows.h>
+
+#define DXGI_OUT
 
 /**
   *	@name		DXGI_getAdapters
@@ -14,7 +17,16 @@
   *	@param[out]	std::vector<IDXGIAdapter*>& adaptersVec 保存所有的Adapter对象，此参数原有的Adapter不会释放。新获取的Adapter对象将会加入到vector的后面
   *	@return		HRESULT S_OK--成功  Other--其他
   */
-HRESULT DXGI_getAdapters(std::vector<IDXGIAdapter*>& adaptersVec);
+HRESULT DXGI_getAdapters(DXGI_OUT std::vector<IDXGIAdapter*>& adaptersVec);
+
+/**
+  *	@name		DXGI_getNearestAdapter
+  * @brief		获取与hWnd的窗口最接近的IDXGIAdapter对象指针
+  *	@param[in]	HWND hWnd 窗口句柄
+  *	@param[out]	IDXGIAdapter** adapter 指向获取到的IDXGIAdapter对象的指针
+  *	@return		HRESULT S_OK--成功  Other--其他
+  */
+HRESULT DXGI_getNearestAdapter(HWND hWnd, DXGI_OUT IDXGIAdapter** adapter);
 
 /**
   *	@name		DXGI_getOutputs
@@ -23,7 +35,7 @@ HRESULT DXGI_getAdapters(std::vector<IDXGIAdapter*>& adaptersVec);
   *	@param[out]	std::vector<IDXGIOutput*>& outputVec 保存所有连接到显卡adapter中的输出设备（显示器），此参数原有的Output不会释放。新获取的Output对象将会加入到vector的后面
   *	@return		HRESULT S_OK--成功  Other--其他
   */
-HRESULT DXGI_getOutputs(IDXGIAdapter* adapter, std::vector<IDXGIOutput*>& outputVec);
+HRESULT DXGI_getOutputs(IDXGIAdapter* adapter, DXGI_OUT std::vector<IDXGIOutput*>& outputVec);
 
 /**
   *	@name		DXGI_releaseAdaptersObjs
@@ -33,6 +45,15 @@ HRESULT DXGI_getOutputs(IDXGIAdapter* adapter, std::vector<IDXGIOutput*>& output
   *	@return		HRESULT S_OK--成功  Other--其他
   */
 HRESULT DXGI_releaseAdaptersObjs(std::vector<IDXGIAdapter*>& adptVec);
+
+/**
+  *	@name		DXGI_releaseAdaptersObjs
+  * @brief		释放Adapter对象
+  *				对每个对象调用release，并将指针置NULL
+  *	@param[in,out]	IDXGIAdapter** adpt 保存所有Adapter（显卡）对象的指针的指针
+  *	@return		HRESULT S_OK--成功  Other--其他
+  */
+HRESULT DXGI_releaseAdapterObj(IDXGIAdapter** adpt);
 
 /**
   *	@name		DXGI_releaseOutputObjs
